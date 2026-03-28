c4cff19e60dc4009221e12b192b503becfc341ab
GastonEzequielArevalo365 <gastonezequielarevalo53@gmail.com>
2026-03-27 23:58:19 -0300

    Se terminaron lo middlewares de productos y se comentaron las lineas explicando las funcionalidades de los mismo

--- src/index.js
+++ src/index.js
@@ -9,14 +9,21 @@ const PORT = 3001
 
 //Solo pones ./routes y automáticamente se lee el index de dicha carpeta.
 const routes = require('./routes')
+const middleware = require('./middlewares')
 
 //Esta línea se pone a nivel app lo que hace es que todas las líneas que se reciban en el body las tome como una JSON
 //app.use quiere decir ejecuta una funciín cuando llega una petición y coincide el path. No responde (a menos que vos lo hagas).
 //En caso de express.json lee el body cuando viene en JSON y lo convierte a un objeto JS accesible en req.body.Si en el body no hay json no hay nada
 app.use(express.json())
 
+//Acá traemos el middleware para el manejo de errores.
+app.use(middleware.errorHandler)
+
 //Lo que hace en este caso es leer lo que hay dentro de cada una de las rutas y si hay alguna coincidencia con el path la app la ejecuta.
 app.use(routes.productosRoute)
+
+//Traemos el middleware que se ocupa de las rutas no encontradas
+app.use(middleware.notFound)
 
 //Iniciamos el servidor y quedamos esperando el servidor solicitudes http.
 //Como tenemos nodemon antes del index.js entiende que cada vez que tenemos un cambio se ejecuta nuevamente index.
